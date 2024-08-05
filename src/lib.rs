222276bd9752bf4fc3758e42b66cba01c5f76d48
Ryan Hedgecock <rhedgeco@gmail.com>
2024-08-05 12:38:34 -0700

    add doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::ops::{Deref, DerefMut};
 
+/// A data container that can offer ownership of its stored information
 pub struct OfferCell<T> {
     item: Option<T>,
 }
@@ -11,23 +12,30 @@ impl<T> Default for OfferCell<T> {
 }
 
 impl<T> OfferCell<T> {
+    /// Returns an empty cell
     pub fn empty() -> Self {
         Self { item: None }
     }
 
+    /// Returns a new cell that stores `item`
     pub fn new(item: T) -> Self {
         Self { item: Some(item) }
     }
 
+    /// Returns a reference to the stored item
     pub fn item(&self) -> Option<&T> {
         self.item.as_ref()
     }
 
+    /// Returns a mutable reference to the stored item
     pub fn item_mut(&mut self) -> Option<&mut T> {
         self.item.as_mut()
     }
 
+    /// Offers optional ownership of the stored data as a [`Offered`] item
     pub fn offer(&mut self) -> Option<Offered<T>> {
+        // offering expects to always hold data
+        // so we must check if there is no data stored first
         if self.item.is_none() {
             return None;
         }
@@ -38,6 +46,7 @@ impl<T> OfferCell<T> {
     }
 }
 
+/// A ownership offering that comes from a [`OfferCell`]
 pub struct Offered<'a, T> {
     item: &'a mut Option<T>,
 }
@@ -63,6 +72,7 @@ impl<'a, T> DerefMut for Offered<'a, T> {
 }
 
 impl<'a, T> Offered<'a, T> {
+    /// Consumes the offering, and takes ownership of the data
     pub fn take(self) -> T {
         match self.item.take() {
             Some(item) => item,
