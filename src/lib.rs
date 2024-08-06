3456a420c8e7f5ff4f094a9cf14856709669c036
Ryan Hedgecock <rhedgeco@gmail.com>
2024-08-05 19:00:07 -0700

    ergonimic improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::ops::{Deref, DerefMut};
 
-/// A data container that can offer ownership of its stored information
+/// A data container that can offer optional ownership of its stored information
 pub struct OfferCell<T> {
     item: Option<T>,
 }
@@ -22,6 +22,11 @@ impl<T> OfferCell<T> {
         Self { item: Some(item) }
     }
 
+    /// Returns true if the cell is empty
+    pub fn is_empty(&self) -> bool {
+        self.item.is_none()
+    }
+
     /// Returns a reference to the stored item
     pub fn item(&self) -> Option<&T> {
         self.item.as_ref()
@@ -32,26 +37,39 @@ impl<T> OfferCell<T> {
         self.item.as_mut()
     }
 
-    /// Offers optional ownership of the stored data as a [`Offered`] item
-    pub fn offer(&mut self) -> Option<Offered<T>> {
+    /// Returns the stored data leaving nothing in its place
+    pub fn take(&mut self) -> Option<T> {
+        self.item.take()
+    }
+
+    /// Inserts `item` into the cell and returns the item as an offering
+    pub fn insert_offer(&mut self, item: T) -> Offering<T> {
+        self.item = Some(item);
+        Offering {
+            item: &mut self.item,
+        }
+    }
+
+    /// Offers optional ownership of the stored data as a [`Offering`] item
+    pub fn offer(&mut self) -> Option<Offering<T>> {
         // offering expects to always hold data
         // so we must check if there is no data stored first
         if self.item.is_none() {
             return None;
         }
 
-        Some(Offered {
+        Some(Offering {
             item: &mut self.item,
         })
     }
 }
 
 /// A ownership offering that comes from a [`OfferCell`]
-pub struct Offered<'a, T> {
+pub struct Offering<'a, T> {
     item: &'a mut Option<T>,
 }
 
-impl<'a, T> Deref for Offered<'a, T> {
+impl<'a, T> Deref for Offering<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -62,7 +80,7 @@ impl<'a, T> Deref for Offered<'a, T> {
     }
 }
 
-impl<'a, T> DerefMut for Offered<'a, T> {
+impl<'a, T> DerefMut for Offering<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         match &mut self.item {
             Some(item) => item,
@@ -71,7 +89,7 @@ impl<'a, T> DerefMut for Offered<'a, T> {
     }
 }
 
-impl<'a, T> Offered<'a, T> {
+impl<'a, T> Offering<'a, T> {
     /// Consumes the offering, and takes ownership of the data
     pub fn take(self) -> T {
         match self.item.take() {
